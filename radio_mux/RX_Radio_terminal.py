527781a2ca2b5094bcb07ba0e1fa859df2f2528d
ttiim <37172333+ttiim@users.noreply.github.com>
2020-11-23 15:11:42 -0800

    Create RX_Radio_terminal.py

---
+++ radio_mux/RX_Radio_terminal.py
@@ -0,0 +1,33 @@
+#Author: Michael Lin
+#Co-authour: Tim Wriglesworth
+
+#Test RX program
+
+#radio_terminal --rx-socket "tcp://127.0.0.1:55555" --tx-socket "tcp://127.0.0.1:44444" --hwid 0102 --raw
+
+
+#!/usr/bin/env python
+import zmq
+import time
+path = "tcp://127.0.0.1:55555"
+context = zmq.Context()
+socket = context.socket(zmq.PUB)
+socket.bind(path)
+print("socket bind success")
+while True:
+    in_str = input()
+    print("send", in_str)
+    socket.send(in_str)		#send data to radio_terminal program 
+
+
+
+# #replacement of the following
+# context = zmq.Context()
+# socket = context.socket(zmq.SUB)
+# socket.connect("tcp://127.0.0.2:5559")   
+# socket.setsockopt_string(zmq.SUBSCRIBE, '')
+# print("running..")
+
+# while True:
+# 	string = socket.recv()
+# 	print(f"Received: {string}")
