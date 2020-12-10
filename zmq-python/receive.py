5deadcc1a5ed0e5d8b9457f25a8d08de046c34f8
ethan-ac <61442798+ethan-ac@users.noreply.github.com>
2020-12-10 15:17:49 -0800

    Add files via upload

---
+++ zmq-python/receive.py
@@ -0,0 +1,37 @@
+#Timothy Wriglesworth
+#ALEASAT Mission
+#ORCASAT Mission
+#University of British Columbia
+
+
+#>python3 receive.py
+
+# to find block implementations look at:
+#https://github.com/daniestevez/gr-satellites/find/maint-3.8
+
+import sys
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+socket.connect("tcp://127.0.0.1:5555")   
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+print("running...")
+
+while True:
+	string = socket.recv()
+	print(f"Received: {string}")
+
+	#Verified sync word: '11010011100100011101001110010001'
+
+
+# 00110101001011100011010100101110
+
+# 00110101001011100011010100101110
+
+
+# 11010011
+# 10010001
+
+
+# 11010011100100011101001110010001
